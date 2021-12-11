cd0ebf8c2bd1b021a2540f70770752db42b637e6
apple <apple@apples-imac-6.attlocal.net>
2021-12-11 15:09:20 -0500

    Committed

---
+++ src/main/java/springboot_kurs_controller_service_repository_basic_authentication/ApplicationSecurityConfig.java
@@ -0,0 +1,62 @@
+package springboot_kurs_controller_service_repository_basic_authentication;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	private final PasswordEncoder passwordEncoder;
+
+	@Autowired
+	public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+		this.passwordEncoder = passwordEncoder;
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.
+			authorizeRequests().
+			antMatchers("/", "index", "/css/*", "/js/*").permitAll().
+			anyRequest().
+			authenticated().
+			and().
+			httpBasic();//Basic authentication istiyorum
+	}
+
+	@Override
+	@Bean
+	protected UserDetailsService userDetailsService() {
+
+		UserDetails student = User.builder().username("techproed").password(passwordEncoder.encode("12345")).roles("STUDENT").build();
+		
+		UserDetails admin = User.builder().username("admin").password(passwordEncoder.encode("nimda")).roles("ADMIN").build();
+		
+		return new InMemoryUserDetailsManager(student, admin);
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
+	
+}
---
+++ src/main/java/springboot_kurs_controller_service_repository_basic_authentication/PasswordConfig.java
@@ -0,0 +1,15 @@
+package springboot_kurs_controller_service_repository_basic_authentication;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class PasswordConfig {
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(10);
+	}
+}
